e35c7350abeacd8a482ddc5bfe148fedd93aecfa
skpe21 <seaneng88+github@gmail.com>
2021-07-08 14:21:36 -0400

    various updates for returning string

--- HelloUniverse.sol
+++ HelloUniverse.sol
@@ -2,4 +2,23 @@ pragma solidity ^0.8.6;
 
 contract HelloUniverse {
 
+  address public ownerAddress;
+
+  constructor() {
+    ownerAddress = msg.sender;
+  }
+
+  function hello() external view returns(string memory) {
+    return "hello";
+  }
+
+  function ownerHello() external view onlyOwner returns(string memory) {
+      return "hello owner";
+  }
+
+  modifier onlyOwner {
+    require(ownerAddress == msg.sender);
+    _;
+  }
+
 }
